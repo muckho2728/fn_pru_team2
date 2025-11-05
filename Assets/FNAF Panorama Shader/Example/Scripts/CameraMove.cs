5ea6683f004d72af23d9b757e357ec04b7d06c7b
LeThanhCong1506 <154421089+LeThanhCong1506@users.noreply.github.com>
2025-11-05 19:40:52 +0700

    Cong has done task
    [add]
    setup gameplay

---
+++ Assets/FNAF Panorama Shader/Example/Scripts/CameraMove.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f79cf3fc85675f88c337fc195117711b14dfaf4f4973accc6bda907ffb088088
+size 1066
---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:870ea5972c986460664d7c33fa71287dc4647f6d35681763d64b539232d6a500
+size 8058
